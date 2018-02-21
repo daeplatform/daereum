30c867e7fa15cce8f6b6691d3ebeba1d20affad2
Dae Platform <36157082+daeplatform@users.noreply.github.com>
2018-02-21 17:59:12 +0700

    Create daereum.sol

---
+++ daereum.sol
@@ -0,0 +1,11 @@
+contract Daereum is StandardToken {
+   string public name = 'Daereum';
+   string public symbol = 'DRT';
+   uint public decimals = 8;
+   uint public INITIAL_SUPPLY = 200000000;
+
+   function Daereum() {
+     totalSupply = INITIAL_SUPPLY;
+     balances[msg.sender] = INITIAL_SUPPLY;
+   }
+}
